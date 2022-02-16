3850edc87a62e2a5477e3225c372c65a8327ad61
Ezzeri Esa <ezzeri.esa@gmail.com>
2022-02-15 17:02:58 -0800

    set up benchmarks

--- src/main.go
+++ src/main.go
@@ -1,158 +1,84 @@
 package main
 
 import (
-	"sort"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
 )
 
-type KeyError struct{}
+const (
+	path   = "/usr/share/dict/words"
+	stride = 8
+	begin  = "echo"
+	end    = "server"
+)
 
-func (e *KeyError) Error() string {
-	return "Key not found"
-}
+func loadWords() ([]string, error) {
+	f, err := os.Open(path)
+	defer f.Close()
 
-type ValueError struct{}
-
-func (e *ValueError) Error() string {
-	return "Inappropriate value"
-}
-
-type DB interface {
-	// Get gets the value for the given key. It returns an error if the
-	// DB does not contain the key.
-	Get(key []byte) (value []byte, err error)
-
-	// Has returns true if the DB contains the given key.
-	Has(key []byte) (ret bool, err error)
-
-	// Put sets the value for the given key. It overwrites any previous value
-	// for that key; a DB is not a multi-map.
-	Put(key, value []byte) error
-
-	// Delete deletes the value for the given key.
-	Delete(key []byte) error
-
-	// RangeScan returns an Iterator (see below) for scanning through all
-	// key-value pairs in the given range, ordered by key ascending.
-	RangeScan(start, limit []byte) (Iterator, error)
-}
-
-type Iterator interface {
-	// Next moves the iterator to the next key/value pair.
-	// It returns false if the iterator is exhausted.
-	Next() bool
-
-	// Error returns any accumulated error. Exhausting all the key/value pairs
-	// is not considered to be an error.
-	Error() error
-
-	// Key returns the key of the current key/value pair, or nil if done.
-	Key() []byte
-
-	// Value returns the value of the current key/value pair, or nil if done.
-	Value() []byte
-}
-
-type SimpleDB struct {
-	store map[string][]byte
-}
-
-func (db SimpleDB) Get(key []byte) (value []byte, err error) {
-	v, ok := db.store[string(key)]
-
-	if !ok {
-		return nil, &KeyError{}
+	if err != nil {
+		return nil, err
 	}
 
-	return v, nil
-}
+	w := make([]string, 0)
+	s := bufio.NewScanner(f)
 
-func (db SimpleDB) Has(key []byte) (ret bool, err error) {
-	_, ok := db.store[string(key)]
-	return ok, nil
-}
-
-func (db SimpleDB) Put(key, value []byte) error {
-	db.store[string(key)] = value
-	return nil
-}
-
-func (db SimpleDB) Delete(key []byte) error {
-	_, ok := db.store[string(key)]
-
-	if !ok {
-		return &KeyError{}
+	for s.Scan() {
+		w = append(w, s.Text())
 	}
 
-	delete(db.store, string(key))
-	return nil
+	return w, nil
 }
 
-func (db SimpleDB) RangeScan(start, limit []byte) (Iterator, error) {
-	strings := make([]string, len(db.store))
-	counter := 0
+func runTest(words []string, db DB, name string) {
+	fmt.Printf("%-20s", name)
 
-	for k, _ := range db.store {
-		strings[counter] = k
-		counter++
+	start := time.Now()
+	for _, word := range words {
+		db.Put([]byte(word), []byte(word))
 	}
+	fmt.Printf("%-20s", time.Since(start))
 
-	sort.Strings(strings)
-
-	startString := string(start)
-	limitString := string(limit)
-
-	if startString > limitString {
-		return nil, &ValueError{}
+	start = time.Now()
+	for i := 0; i < len(words); i += stride {
+		db.Delete([]byte(words[i]))
 	}
+	fmt.Printf("%-20s", time.Since(start))
 
-	keys := make([][]byte, 0)
-	values := make([][]byte, 0)
+	start = time.Now()
+	for _, word := range words {
+		db.Get([]byte(word))
+	}
+	fmt.Printf("%-20s", time.Since(start))
 
-	for _, key := range strings {
-		if key >= startString && key < limitString {
-			keys = append(keys, []byte(key))
-			values = append(values, []byte(db.store[key]))
+	start = time.Now()
+	r, _ := db.RangeScan([]byte(begin), []byte(end))
+	for {
+		r.Key()
+		r.Value()
+
+		hasNext := r.Next()
+		if !hasNext {
+			break
 		}
 	}
-
-	return &SimpleIterator{
-		keys:   keys,
-		values: values,
-		index:  0,
-	}, nil
+	fmt.Printf("%-20s", time.Since(start))
 }
 
-type SimpleIterator struct {
-	keys   [][]byte
-	values [][]byte
-	index  int
-}
+func main() {
+	words, err := loadWords()
 
-func (s *SimpleIterator) Next() bool {
-	if s.index == len(s.keys) - 1 {
-		return false
+	if err != nil {
+		log.Fatalf("Error: loading words\n")
 	}
 
-	s.index++
-	return true
-}
+	store := make(map[string][]byte, 0)
+	db := SimpleDB{store: store}
 
-func (s *SimpleIterator) Error() error {
-	return nil
-}
-
-func (s *SimpleIterator) Key() []byte {
-	if len(s.keys) == 0 {
-		return nil
-	}
-
-	return s.keys[s.index]
-}
-
-func (s *SimpleIterator) Value() []byte {
-	if len(s.values) == 0 {
-		return nil
-	}
-
-	return s.values[s.index]
+	fmt.Printf("%-20s%-20s%-20s%-20s%-20s\n", "name", "puts", "deletes", "gets", "rangescan")
+	runTest(words, db, "simple")
+	fmt.Printf("\n")
 }
---
+++ src/memtable.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"sort"
+)
+
+type KeyError struct{}
+
+func (e *KeyError) Error() string {
+	return "Key not found"
+}
+
+type ValueError struct{}
+
+func (e *ValueError) Error() string {
+	return "Inappropriate value"
+}
+
+type DB interface {
+	// Get gets the value for the given key. It returns an error if the
+	// DB does not contain the key.
+	Get(key []byte) (value []byte, err error)
+
+	// Has returns true if the DB contains the given key.
+	Has(key []byte) (ret bool, err error)
+
+	// Put sets the value for the given key. It overwrites any previous value
+	// for that key; a DB is not a multi-map.
+	Put(key, value []byte) error
+
+	// Delete deletes the value for the given key.
+	Delete(key []byte) error
+
+	// RangeScan returns an Iterator (see below) for scanning through all
+	// key-value pairs in the given range, ordered by key ascending.
+	RangeScan(start, limit []byte) (Iterator, error)
+}
+
+type Iterator interface {
+	// Next moves the iterator to the next key/value pair.
+	// It returns false if the iterator is exhausted.
+	Next() bool
+
+	// Error returns any accumulated error. Exhausting all the key/value pairs
+	// is not considered to be an error.
+	Error() error
+
+	// Key returns the key of the current key/value pair, or nil if done.
+	Key() []byte
+
+	// Value returns the value of the current key/value pair, or nil if done.
+	Value() []byte
+}
+
+type SimpleDB struct {
+	store map[string][]byte
+}
+
+func (db SimpleDB) Get(key []byte) (value []byte, err error) {
+	v, ok := db.store[string(key)]
+
+	if !ok {
+		return nil, &KeyError{}
+	}
+
+	return v, nil
+}
+
+func (db SimpleDB) Has(key []byte) (ret bool, err error) {
+	_, ok := db.store[string(key)]
+	return ok, nil
+}
+
+func (db SimpleDB) Put(key, value []byte) error {
+	db.store[string(key)] = value
+	return nil
+}
+
+func (db SimpleDB) Delete(key []byte) error {
+	_, ok := db.store[string(key)]
+
+	if !ok {
+		return &KeyError{}
+	}
+
+	delete(db.store, string(key))
+	return nil
+}
+
+func (db SimpleDB) RangeScan(start, limit []byte) (Iterator, error) {
+	strings := make([]string, len(db.store))
+	counter := 0
+
+	for k, _ := range db.store {
+		strings[counter] = k
+		counter++
+	}
+
+	sort.Strings(strings)
+
+	startString := string(start)
+	limitString := string(limit)
+
+	if startString > limitString {
+		return nil, &ValueError{}
+	}
+
+	keys := make([][]byte, 0)
+	values := make([][]byte, 0)
+
+	for _, key := range strings {
+		if key >= startString && key < limitString {
+			keys = append(keys, []byte(key))
+			values = append(values, []byte(db.store[key]))
+		}
+	}
+
+	return &SimpleIterator{
+		keys:   keys,
+		values: values,
+		index:  0,
+	}, nil
+}
+
+type SimpleIterator struct {
+	keys   [][]byte
+	values [][]byte
+	index  int
+}
+
+func (s *SimpleIterator) Next() bool {
+	if s.index == len(s.keys)-1 {
+		return false
+	}
+
+	s.index++
+	return true
+}
+
+func (s *SimpleIterator) Error() error {
+	return nil
+}
+
+func (s *SimpleIterator) Key() []byte {
+	if len(s.keys) == 0 {
+		return nil
+	}
+
+	return s.keys[s.index]
+}
+
+func (s *SimpleIterator) Value() []byte {
+	if len(s.values) == 0 {
+		return nil
+	}
+
+	return s.values[s.index]
+}
