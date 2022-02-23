20e0f8b42d29cb9f695e61049c408558e2653b27
Ezzeri Esa <ezzeri.esa@gmail.com>
2022-02-22 17:19:46 -0800

    implement rangescan for immutable db

--- src/table.go
+++ src/table.go
@@ -88,9 +88,9 @@ func Open(r ReaderSeeker) (*Table, error) {
 
 func (t Table) getBlock(key []byte) (offsetStart, offsetEnd uint32, isOffset bool) {
 	for i := 1; i < len(t.sparseIndex); i++ {
-		endKey := t.sparseIndex[i].key
+		keyEnd := t.sparseIndex[i].key
 
-		if string(key) < string(endKey) {
+		if string(key) < string(keyEnd) {
 			return t.sparseIndex[i-1].offset, t.sparseIndex[i].offset, true
 		}
 	}
@@ -167,4 +167,96 @@ func (t Table) Get(key []byte) (value []byte, err error) {
 func (t Table) Has(key []byte) (ret bool, err error) {
 	_, e := t.Get(key)
 	return e == nil, nil
+}
+
+func (t Table) RangeScan(start, limit []byte) (Iterator, error) {
+	startString := string(start)
+	limitString := string(limit)
+
+	if startString > limitString {
+		return nil, ValueError
+	}
+
+	keyStart := t.sparseIndex[0].key
+	keyEnd := t.sparseIndex[len(t.sparseIndex)-1].key
+
+	keys := make([][]byte, 0)
+	values := make([][]byte, 0)
+
+	if startString > string(keyEnd) || limitString < string(keyStart) {
+		return &SimpleIterator{
+			keys:   keys,
+			values: values,
+			index:  0,
+		}, nil
+	}
+
+	offsetStart, _, _ := t.getBlock(start)
+	_, offsetEnd, _ := t.getBlock(limit)
+
+	reader := io.NewSectionReader(t.reader, int64(offsetStart), int64(offsetEnd-offsetStart))
+	isTracking := false
+
+	for {
+		var keyLength uint32
+		err := binary.Read(reader, binary.LittleEndian, &keyLength)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, fmt.Errorf("reading key length: %s", err)
+		}
+
+		key := make([]byte, keyLength)
+		_, err = reader.Read(key)
+		if err != nil {
+			if err == io.EOF {
+				return nil, fmt.Errorf("corrupted block: end of block while reading key")
+			}
+
+			return nil, fmt.Errorf("reading key: %s", err)
+		}
+
+		if !isTracking {
+			if startString >= string(key) {
+				isTracking = true
+			} else {
+				continue
+			}
+		} else {
+			if string(key) > limitString {
+				break
+			}
+		}
+
+		var valueLength uint32
+		err = binary.Read(reader, binary.LittleEndian, &valueLength)
+		if err != nil {
+			if err == io.EOF {
+				return nil, ValueError
+			}
+
+			return nil, fmt.Errorf("reading value length: %s", err)
+		}
+
+		value := make([]byte, valueLength)
+		_, err = reader.Read(value)
+		if err != nil {
+			if err == io.EOF {
+				return nil, fmt.Errorf("corrupted block: end of block while reading value")
+			}
+
+			return nil, fmt.Errorf("reading value: %s", err)
+		}
+
+		keys = append(keys, key)
+		values = append(values, value)
+	}
+
+	return &SimpleIterator{
+		keys:   keys,
+		values: values,
+		index:  0,
+	}, nil
 }
